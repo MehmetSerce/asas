f55b487f454b61264f20cd976658298bef6ac258
MehmetSerce <mehmetserce85@hotmail.com>
2017-09-05 03:08:58 +0300

    dgfdgfdg

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-            //deneme239845
+            //deneme23984590
             MessageBox.Show("merhaba");
 		}
 	}
