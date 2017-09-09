94e2246649334c213f92f70b4ee8d4fbf278e482
MehmetSerce <mehmetserce85@hotmail.com>
2017-09-09 22:55:00 +0300

    test ekle

--- Testiing/UnitTest.cs
+++ Testiing/UnitTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace Testiing
 {
+    [TestFixture]
     public class UnitTest
     {
+        [TestCase]
+        public void test1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestCase]
+        public void test2()
+        {
+            Assert.AreEqual(1, 1);
+        }
     }
 }
