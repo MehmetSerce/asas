fcc337b982890e48a422492a51e3bdd70afe8ded
MehmetSerce <mehmetserce85@hotmail.com>
2017-09-09 22:21:15 +0300

    nunit deneme

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -20,7 +20,7 @@ namespace WindowsFormsApp1
 		private void button1_Click(object sender, EventArgs e)
 		{
             //deneme2398459090
-            MessageBox.Show("merhaba")
+            MessageBox.Show("merhaba");
 		}
 	}
 }
