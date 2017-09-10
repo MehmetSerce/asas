445ecc63c06cd7a20eee0fa46492aa867e603a1a
MehmetSerce <mehmetserce85@hotmail.com>
2017-09-10 03:47:30 +0300

    added classlibrary for readtxtFile

--- Testiing/UnitTest.cs
+++ Testiing/UnitTest.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XmlOku;
 
 namespace Testiing
 {
@@ -32,27 +33,7 @@ namespace Testiing
         [TestCase]
         public void test2()
         {
-            string dosya_yolu = "C:\\projects\\asas\\Testiing\\TextFile1.txt";
-            //string dosya_yolu = "C:\\Users\\User\\source\\repos\\WindowsFormsApp1\\Testiing\\TextFile1.txt";
-        
-         //Okuma işlem yapacağımız dosyanın yolunu belirtiyoruz.
-         FileStream fs = new FileStream(dosya_yolu, FileMode.Open, FileAccess.Read);
-            //Bir file stream nesnesi oluşturuyoruz. 1.parametre dosya yolunu,
-            //2.parametre dosyanın açılacağını,
-            //3.parametre dosyaya erişimin veri okumak için olacağını gösterir.
-            StreamReader sw = new StreamReader(fs);
-            //Okuma işlemi için bir StreamReader nesnesi oluşturduk.
-            string yazi = sw.ReadLine();
-            string sakla = yazi;
-            while (yazi != null)
-            {
-                Console.WriteLine(yazi);
-                yazi = sw.ReadLine();
-            }
-            //Satır satır okuma işlemini gerçekleştirdik ve ekrana yazdırdık
-            //Son satır okunduktan sonra okuma işlemini bitirdik
-            sw.Close();
-            fs.Close();
+         string sakla =   readTxt.readtxtfile();
             Assert.AreEqual(1, sakla);
         }
     }
---
+++ XmlOku/readTxt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlOku
+{
+    public class readTxt
+    {
+        public static string readtxtfile()
+        {
+             //string dosya_yolu = "C:\\projects\\asas\\Testiing\\TextFile1.txt";
+           string dosya_yolu = "C:\\Users\\User\\source\\repos\\WindowsFormsApp1\\Testiing\\TextFile1.txt";
+
+            //Okuma işlem yapacağımız dosyanın yolunu belirtiyoruz.
+            FileStream fs = new FileStream(dosya_yolu, FileMode.Open, FileAccess.Read);
+            //Bir file stream nesnesi oluşturuyoruz. 1.parametre dosya yolunu,
+            //2.parametre dosyanın açılacağını,
+            //3.parametre dosyaya erişimin veri okumak için olacağını gösterir.
+            StreamReader sw = new StreamReader(fs);
+            //Okuma işlemi için bir StreamReader nesnesi oluşturduk.
+            string yazi = sw.ReadLine();
+            string sakla = yazi;
+            while (yazi != null)
+            {
+                Console.WriteLine(yazi);
+                yazi = sw.ReadLine();
+            }
+            //Satır satır okuma işlemini gerçekleştirdik ve ekrana yazdırdık
+            //Son satır okunduktan sonra okuma işlemini bitirdik
+            sw.Close();
+            fs.Close();
+            return sakla;
+        }
+    }
+}
