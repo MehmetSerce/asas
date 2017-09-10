809a045251dbfe81e6115160c9b3858336d83e51
MehmetSerce <mehmetserce85@hotmail.com>
2017-09-10 03:33:00 +0300

    for txt file

--- Testiing/UnitTest.cs
+++ Testiing/UnitTest.cs
@@ -1,8 +1,11 @@
-﻿using NHtmlUnit.Html;
+﻿
+using NHtmlUnit;
+using NHtmlUnit.Html;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,13 +18,13 @@ namespace Testiing
         [TestCase]
         public void test1()
         {
-            NHtmlUnit.WebClient driver = new NHtmlUnit.WebClient();
-           
+            NHtmlUnit.WebClient driver=null;
+            driver = new NHtmlUnit.WebClient(BrowserVersion.INTERNET_EXPLORER_11);
             driver.Options.JavaScriptEnabled = true;
            driver.Options.ThrowExceptionOnScriptError = false;
             driver.Options.ActiveXNative = true;
             driver.Options.CssEnabled = true;
-            HtmlPage page = driver.GetHtmlPage("https://www.sahibinden.com/");
+            HtmlPage page = driver.GetHtmlPage("https://www.expedia.com/");
 
             Assert.AreEqual("Google", page.TitleText);
         }
@@ -29,6 +32,26 @@ namespace Testiing
         [TestCase]
         public void test2()
         {
+            string dosya_yolu = "C:\\projects\\asas\\Testiing\\TextFile1.txt";
+            //string dosya_yolu = "C:\\Users\\User\\source\\repos\\WindowsFormsApp1\\Testiing\\TextFile1.txt";
+        
+         //Okuma işlem yapacağımız dosyanın yolunu belirtiyoruz.
+         FileStream fs = new FileStream(dosya_yolu, FileMode.Open, FileAccess.Read);
+            //Bir file stream nesnesi oluşturuyoruz. 1.parametre dosya yolunu,
+            //2.parametre dosyanın açılacağını,
+            //3.parametre dosyaya erişimin veri okumak için olacağını gösterir.
+            StreamReader sw = new StreamReader(fs);
+            //Okuma işlemi için bir StreamReader nesnesi oluşturduk.
+            string yazi = sw.ReadLine();
+            while (yazi != null)
+            {
+                Console.WriteLine(yazi);
+                yazi = sw.ReadLine();
+            }
+            //Satır satır okuma işlemini gerçekleştirdik ve ekrana yazdırdık
+            //Son satır okunduktan sonra okuma işlemini bitirdik
+            sw.Close();
+            fs.Close();
             Assert.AreEqual(1, 1);
         }
     }
